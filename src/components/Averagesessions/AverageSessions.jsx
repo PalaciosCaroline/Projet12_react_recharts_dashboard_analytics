58ba39dfde8a167c4adcd89a56c50d36c46356ae
PalaciosCaroline <caroline2807@yahoo.fr>
2023-01-24 17:28:54 +0100

    Nettoyage

--- src/components/Averagesessions/AverageSessions.jsx
+++ src/components/Averagesessions/AverageSessions.jsx
@@ -76,6 +76,7 @@ export default function AverageSessions({dataAverageSessions}) {
 }
 
 AverageSessions.propTypes = {
+  dataAverageSessions : PropTypes.object,
   userAverageSessions : PropTypes.array,
   TooltipTagAverageSessions : PropTypes.element,
   active: PropTypes.bool,
